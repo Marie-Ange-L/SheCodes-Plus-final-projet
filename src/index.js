2e845a5336347a4e1e1a0627eb5c486fdae257f5
Marie-Ange-L <76390117+Marie-Ange-L@users.noreply.github.com>
2021-02-28 16:56:39 +0100

    Connected API for Search form & current location + Celsius/Farenheit display

--- src/index.js
+++ src/index.js
@@ -75,3 +75,69 @@ day5.innerHTML = formatShortDate5();
 
 // Date functions - END
 
+
+
+
+// Search API 
+
+function getWeather(response) {
+  document.querySelector("#current-city-1").innerHTML = response.data.name;
+  document.querySelector("#current-city-2").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp) + "°C";;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  celsiusTemperature = response.data.main.temp;
+}
+
+function searchCity(city) {
+    let apiKey = "c3a45ccf88ff19c1e83029ef1f84c87f";
+  let apiDomain = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiDomain}?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getWeather);
+}
+
+function refresh(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "0b05732c31f31d299fde388ef85a7016";
+  let apiDomain = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiDomain}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getWeather);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", refresh);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Lille");
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let farenheitTemperature = (celsiusTemperature *9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(farenheitTemperature) + "°F";
+ }
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature) + "°C";
+ }
+
+
+let farenheitLink = document.querySelector("#farenheit-link");
+farenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
